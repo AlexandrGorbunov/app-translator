7193397d9fdab419ed7f190c94435e6506c080c4
AlexandrGorbunov <108086875+AlexandrGorbunov@users.noreply.github.com>
2023-05-03 22:06:19 +0300

    Add files via upload

---
+++ precache-manifest.177f0a39bd86bf8e4402b5cb2d0401fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d01c77c735dfab8e1a3af71a4de4c62",
+    "url": "/index.html"
+  },
+  {
+    "revision": "113eb936f5cf18da5421",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "3a37c87d825212c9acf3",
+    "url": "/static/js/2.00fb7832.chunk.js"
+  },
+  {
+    "revision": "113eb936f5cf18da5421",
+    "url": "/static/js/main.6c0b5e2f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
